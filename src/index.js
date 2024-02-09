f04839dab9d695856b8fc1206b1b78b93920a3ac
sillerdis <sille.dammann@gmail.com>
2024-02-09 10:38:26 +0200

    all

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ const common = require('./common.js');
 common.hello();
 import esmodule from './esmodule.js';
 esmodule.hello();
+console.log('Hello HMR!');
+import './style.css';
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+
+    module.exports = {
+        
+        module: {
+          rules: [
+            {
+              test: /\.css$/i,
+              use: ["style-loader", "css-loader"],
+            },
+          ],
+        },
+      
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'about.html'
+        })
+    ],
+};
